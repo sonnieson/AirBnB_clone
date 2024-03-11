4c40f3ddebf13e46f70bd5cdf2b88b3302591e86
Faith <faykimanthi@gmail.com>
2024-03-10 19:00:33 -0700

    he

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines the State class."""
+"""This defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
+    """This represents a state.
 
     Attributes:
         name (str): The name of the state.
