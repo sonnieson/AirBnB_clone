4f39999d49357b7bd546b698f0a7dfcb1af58721
sonnison <mutiriaesther9@gmail.com>
2024-03-11 03:51:08 +0300

    Committing chnages to test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1188,7 +1188,7 @@ class TestHBNBCommand_update(unittest.TestCase):
         testCmd = "update BaseModel {} attr_name 'attr_value'".format(testId)
         self.assertFalse(HBNBCommand().onecmd(testCmd))
         test_dict = storage.all()["BaseModel.{}".format(testId)].__dict__
-        self.assertEqual("attr_value", test_dict["attr_name"])
+        self.assertEqual("'attr_value'", test_dict["attr_name"])
 
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd("create User")
