5ff24b9517cdd974fbffc9f5249f97b6e4dc5de6
Faith <faykimanthi@gmail.com>
2024-03-10 18:58:29 -0700

    he

--- models/review.py
+++ models/review.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""This defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
+    """This represents a review.
 
     Attributes:
         place_id (str): The Place id.
