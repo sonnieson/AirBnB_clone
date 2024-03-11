12fb1f989b3c5254cceb4d69674a380e7de8e1c8
Faith <faykimanthi@gmail.com>
2024-03-10 18:41:46 -0700

    he

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""This defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
+    """This represent an amenity.
 
     Attributes:
         name (str): The name of the amenity.
