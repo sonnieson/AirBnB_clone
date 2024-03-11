f4d83c1579be16346bccd163538c4423af44030f
Faith <faykimanthi@gmail.com>
2024-03-10 19:02:56 -0700

    he

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""This defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
+    """This represents a User.
 
     Attributes:
         email (str): The email of the user.
